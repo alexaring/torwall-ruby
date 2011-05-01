4bf468cb46809aa62363f73ef9c89fc2d52e9b27
alexander <alex@x61s>
2011-05-01 02:11:25 +0200

    Added tortray ruby skript and some icons...

---
+++ rtortray/rtortray.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby1.8
+#
+#
+
+require 'gtk2'
+
+tray = Gtk::StatusIcon.new
+icon = Gdk::Pixbuf.new("./pixmaps/torwallActive.png",32,32)
+tray.pixbuf=icon
+tray.tooltip=("Torwall Tray")
+tray.visible=(true)
+
+Gtk.main
